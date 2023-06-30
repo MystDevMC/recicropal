a8f6ab948613a0d01f582d31dbd4c4666220aed9
MystDevMc <mystdevmc@gmail.com>
2023-06-30 14:50:54 +0700

    Fix minor placement issue

--- src/main/java/com/mystdev/recicropal/crop/bottle_gourd/BottleGourdItem.java
+++ src/main/java/com/mystdev/recicropal/crop/bottle_gourd/BottleGourdItem.java
@@ -27,80 +27,87 @@ public class BottleGourdItem extends BlockItem {
     }
 
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        var blockhitresult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.ANY);
-        var pos = blockhitresult.getBlockPos();
-        var fluid = level.getFluidState(pos);
-        var isSneaking = player.isCrouching();
 
         var stack = player.getItemInHand(hand);
         var usedStack = ItemHandlerHelper.copyStackWithSize(stack, 1);
 
-        if (!fluid.isEmpty() && fluid.isSource() && !isSneaking) {
-            var result = FluidUtil.tryPickUpFluid(usedStack,
-                                                  player,
-                                                  level,
-                                                  pos,
-                                                  blockhitresult.getDirection());
-            if (result.isSuccess()) {
-                level.playLocalSound(pos.getX(),
-                                     pos.getY(),
-                                     pos.getZ(),
-                                     SoundEvents.BOTTLE_FILL,
-                                     SoundSource.BLOCKS,
-                                     1,
-                                     1,
-                                     false);
-                if (!player.getAbilities().instabuild) {
-                    if (stack.getCount() != 1) {
-                        if (!player.addItem(result.result))
-                            ItemHandlerHelper.giveItemToPlayer(player, result.result);
-                        player.setItemInHand(hand, ItemHandlerHelper.copyStackWithSize(stack, stack.getCount() - 1));
-                    }
-                    else {
-                        player.setItemInHand(hand, result.result);
-                    }
-                }
-                return InteractionResultHolder.success(player.getItemInHand(hand));
-            }
-            return InteractionResultHolder.fail(player.getItemInHand(hand));
+        var blockhitresult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.ANY);
+
+        var isSneaking = player.isCrouching();
+        if (isSneaking) {
+            blockhitresult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.NONE);
         }
-        else if (!isSneaking) {
-            var targetPos = pos.relative(blockhitresult.getDirection());
-            if (!fluid.isEmpty()) targetPos = pos;
-            var result = FluidUtil.tryPlaceFluid(player,
-                                                 level,
-                                                 hand,
-                                                 targetPos,
-                                                 player.getItemInHand(hand),
-                                                 FluidUtil.getFluidContained(player.getItemInHand(hand)).orElse(
-                                                         FluidStack.EMPTY));
-            if (result.isSuccess()) {
-                level
-                        .playLocalSound(pos.getX(),
-                                        pos.getY(),
-                                        pos.getZ(),
-                                        SoundEvents.BOTTLE_EMPTY,
-                                        SoundSource.BLOCKS,
-                                        1,
-                                        1,
-                                        false);
-                if (!player.getAbilities().instabuild) {
-                    if (stack.getCount() != 1) {
-                        if (!player.addItem(result.result))
-                            ItemHandlerHelper.giveItemToPlayer(player, result.result);
-                        player.setItemInHand(hand, ItemHandlerHelper.copyStackWithSize(stack, stack.getCount() - 1));
+
+        var pos = blockhitresult.getBlockPos();
+        var fluid = level.getFluidState(pos);
+
+        if (!isSneaking) {
+            if (!fluid.isEmpty() && fluid.isSource()) {
+                var result = FluidUtil.tryPickUpFluid(usedStack,
+                                                      player,
+                                                      level,
+                                                      pos,
+                                                      blockhitresult.getDirection());
+                if (result.isSuccess()) {
+                    level.playLocalSound(pos.getX(),
+                                         pos.getY(),
+                                         pos.getZ(),
+                                         SoundEvents.BOTTLE_FILL,
+                                         SoundSource.BLOCKS,
+                                         1,
+                                         1,
+                                         false);
+                    if (!player.getAbilities().instabuild) {
+                        if (stack.getCount() != 1) {
+                            if (!player.addItem(result.result))
+                                ItemHandlerHelper.giveItemToPlayer(player, result.result);
+                            player.setItemInHand(hand, ItemHandlerHelper.copyStackWithSize(stack, stack.getCount() - 1));
+                        }
+                        else {
+                            player.setItemInHand(hand, result.result);
+                        }
                     }
-                    else {
-                        player.setItemInHand(hand, result.result);
-                    }
+                    return InteractionResultHolder.success(player.getItemInHand(hand));
                 }
-                return InteractionResultHolder.success(player.getItemInHand(hand));
+                return InteractionResultHolder.fail(player.getItemInHand(hand));
+            }
+            else {
+                var targetPos = pos.relative(blockhitresult.getDirection());
+                if (!fluid.isEmpty()) targetPos = pos;
+                var result = FluidUtil.tryPlaceFluid(player,
+                                                     level,
+                                                     hand,
+                                                     targetPos,
+                                                     player.getItemInHand(hand),
+                                                     FluidUtil.getFluidContained(player.getItemInHand(hand)).orElse(
+                                                             FluidStack.EMPTY));
+                if (result.isSuccess()) {
+                    level
+                            .playLocalSound(pos.getX(),
+                                            pos.getY(),
+                                            pos.getZ(),
+                                            SoundEvents.BOTTLE_EMPTY,
+                                            SoundSource.BLOCKS,
+                                            1,
+                                            1,
+                                            false);
+                    if (!player.getAbilities().instabuild) {
+                        if (stack.getCount() != 1) {
+                            if (!player.addItem(result.result))
+                                ItemHandlerHelper.giveItemToPlayer(player, result.result);
+                            player.setItemInHand(hand, ItemHandlerHelper.copyStackWithSize(stack, stack.getCount() - 1));
+                        }
+                        else {
+                            player.setItemInHand(hand, result.result);
+                        }
+                    }
+                    return InteractionResultHolder.success(player.getItemInHand(hand));
+                }
             }
         }
 
         var clickedState = level.getBlockState(blockhitresult
                                                        .getBlockPos());
-
         if (!clickedState.isAir()) {
             InteractionResult interactionresult = super.useOn(new UseOnContext(player, hand, blockhitresult));
             return new InteractionResultHolder<>(interactionresult, player.getItemInHand(hand));
